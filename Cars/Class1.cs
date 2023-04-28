c4ce0d8a802a6a3386fcbf167103415691ebe752
mafe <Usuario@DESKTOP-PDI6DA1>
2023-04-28 18:13:18 -0500

    Add project files.

---
+++ Cars/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace Cars
+{
+    public class Cars
+    {
+        public int Brand { get; set; }
+        public int Model { get; set; }
+        public int Year { get; set; }
+        public int Color { get; set; }
+        public int Price { get; set; }
+
+    }
+}
